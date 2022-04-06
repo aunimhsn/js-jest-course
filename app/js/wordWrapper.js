47bcf6e9bc1d83008d980f2d4784d79c1e90e978
Aunim HASSAN <aunim.hassan@gmail.com>
2022-04-06 22:47:46 +0200

    feat: word wrapper

---
+++ app/js/wordWrapper.js
@@ -0,0 +1,57 @@
+var Wrapper = function(text, columnNum) {
+    this.text = text;
+    this.columnNum = columnNum;
+};
+
+Wrapper.prototype._areParamTypesValidForWrapFunction = function() {
+    return (typeof(this.text) == "string") && (typeof(this.columnNum) == "number") && (this.columnNum > 0);
+};
+
+Wrapper.prototype._calculateLastBlankPos = function(currentText) {
+    var finished = false;
+    var lastBlankPos = null;
+    for (var i=0; i<this.columnNum+1; i++) {
+        if (currentText.length > i) {
+            if (currentText[i] == ' ') {
+                lastBlankPos = i;
+            }
+        }
+        else {
+            finished = true;
+        }
+    }
+    return {finished: finished, lastBlankPos: lastBlankPos};
+};
+
+Wrapper.prototype._processingText = function(state, strings) {
+    if ((!state.finished) && (state.lastBlankPos != null)) {
+        strings.finalText = strings.finalText.concat(strings.tmpText.substr(0, state.lastBlankPos) + '\n');
+        strings.tmpText = strings.tmpText.substr(state.lastBlankPos + 1, strings.tmpText.length);
+    }
+    else if (state.finished) {
+        strings.finalText = strings.finalText.concat(strings.tmpText.substr(0, strings.tmpText.length));
+    }
+    else {
+        strings.finalText = strings.finalText.concat(strings.tmpText.substr(0, this.columnNum) + '\n');
+        strings.tmpText = strings.tmpText.substr(this.columnNum, strings.tmpText.length);
+    }
+    return strings;
+};
+
+Wrapper.wrap = function(text, columnNum) {
+    var wrapper = new Wrapper(text, columnNum);
+    if (wrapper._areParamTypesValidForWrapFunction()) {
+        var strings = {tmpText: wrapper.text, finalText: ""};
+        var state = {finished: false, lastBlankPos: null};
+        while (!state.finished) {
+            state = wrapper._calculateLastBlankPos(strings.tmpText);
+            strings = wrapper._processingText(state, strings);
+        }
+        return strings.finalText;
+    }
+    else {
+        return "error";
+    }
+};
+
+console.log(Wrapper.wrap("Hello World!", 3));
