3fddc24d4d529a8d615b12b6cb5c14ec40d5f2ad
Aunim HASSAN <aunim.hassan@gmail.com>
2022-04-08 01:12:06 +0200

    feat: leap years in progress

---
+++ app/js/isLeapYear.js
@@ -0,0 +1,17 @@
+/**
+ - 
+
+ -
+ -        
+ */
+
+function isLeapYear(year) {
+	if ((year % 400 === 0) && (year % 100 === 0))
+		return true;
+	else if ((year % 4 === 0) && (year % 100 !== 0))
+		return true;
+	else
+		return false;
+}
+
+export { isLeapYear }
---
+++ app/tests/leapYears.test.js
@@ -0,0 +1,25 @@
+/**
+ - Leap Years - Les Années Bisextiles :
+   
+   Écrire une fonction isLeapYear() qui renvoie VRAI ou FAUX
+   selon si l'année passé en paramètre est bisextile ou non.
+   
+   Sachant que :
+		- Une année est bisextile si elle est divisible par 4 mais
+		  pas divisible par 100 sauf si elle est aussi divisible par 400
+
+	Exemples :
+		- 2001 est une année typique
+		- 1996 est une année bisextile typique
+		- 1900 est une année normale atypique
+		- 2000 est une année bisextile atypique
+
+ -
+ -        
+ */
+
+describe('Leap Years', () => {
+
+});
+
+export { isLeapYears }
